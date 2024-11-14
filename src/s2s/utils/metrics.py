c5631be711d08683a720f7c484b0eb1c012ce97c
khalilv <khalil.virji1@gmail.com>
2024-11-13 19:19:03 -0700

    updated metrics to do intermediate computations

--- src/s2s/utils/metrics.py
+++ src/s2s/utils/metrics.py
@@ -4,37 +4,34 @@
 import numpy as np
 import torch
 from torchmetrics import Metric
-from torchmetrics.utilities.data import dim_zero_cat
-from s2s.utils.data_utils import encode_timestamp, decode_timestamp, remove_year
 
 class mse(Metric):
     def __init__(self, vars, transforms=None, suffix=None, **kwargs):
         super().__init__(**kwargs)
-        self.add_state("preds", default=[], dist_reduce_fx="cat")
-        self.add_state("targets", default=[], dist_reduce_fx="cat")
+        self.add_state("mse_over_hw_sum", default=torch.zeros(len(vars)), dist_reduce_fx="sum")
+        self.add_state("count", default=torch.tensor(0), dist_reduce_fx="sum")
         self.vars = vars
         self.transforms = transforms
         self.suffix = suffix
 
     def update(self, preds: torch.Tensor, targets: torch.Tensor):
-        self.preds.append(preds)
-        self.targets.append(targets)
-
-    def compute(self):
-        preds = dim_zero_cat(self.preds)
-        targets = dim_zero_cat(self.targets)
-        
         if self.transforms is not None:
             preds = self.transforms(preds)
-            targets = self.transforms(targets) 
-
+            targets = self.transforms(targets)
+        
+        B = preds.shape[0]          
         error = (preds - targets) ** 2
+        mse_over_hw = error.mean(dim=(2,3))
 
+        self.mse_over_hw_sum += mse_over_hw.sum(dim=0)
+        self.count += B
+
+    def compute(self):
         loss_dict = {}
 
         for i, var in enumerate(self.vars):
             var_loss_name = f'mse_{var}_{self.suffix}' if self.suffix else f'mse_{var}'
-            loss_dict[var_loss_name] = error[:, i].mean()
+            loss_dict[var_loss_name] = self.mse_over_hw_sum[i] / self.count
         
         loss_name = f'mse_{self.suffix}' if self.suffix else f'mse'
         loss_dict[loss_name] = torch.mean(torch.stack(list(loss_dict.values())))
@@ -44,31 +41,30 @@ class mse(Metric):
 class rmse(Metric):
     def __init__(self, vars, transforms=None, suffix=None, **kwargs):
         super().__init__(**kwargs)
-        self.add_state("preds", default=[], dist_reduce_fx="cat")
-        self.add_state("targets", default=[], dist_reduce_fx="cat")
+        self.add_state("mse_over_hw_sum", default=torch.zeros(len(vars)), dist_reduce_fx="sum")
+        self.add_state("count", default=torch.tensor(0), dist_reduce_fx="sum")
         self.vars = vars
         self.transforms = transforms
         self.suffix = suffix
 
     def update(self, preds: torch.Tensor, targets: torch.Tensor):
-        self.preds.append(preds)
-        self.targets.append(targets)
-
-    def compute(self):
-        preds = dim_zero_cat(self.preds)
-        targets = dim_zero_cat(self.targets)
-        
         if self.transforms is not None:
             preds = self.transforms(preds)
-            targets = self.transforms(targets) 
-
+            targets = self.transforms(targets)
+        
+        B = preds.shape[0]          
         error = (preds - targets) ** 2
+        mse_over_hw = error.mean(dim=(2,3))
 
+        self.mse_over_hw_sum += mse_over_hw.sum(dim=0)
+        self.count += B
+
+    def compute(self):
         loss_dict = {}
 
         for i, var in enumerate(self.vars):
             var_loss_name = f'rmse_{var}_{self.suffix}' if self.suffix else f'rmse_{var}'
-            loss_dict[var_loss_name] = torch.sqrt(error[:, i].mean())
+            loss_dict[var_loss_name] = torch.sqrt(self.mse_over_hw_sum[i] / self.count)
 
         loss_name = f'rmse_{self.suffix}' if self.suffix else f'rmse'
         loss_dict[loss_name] = torch.mean(torch.stack(list(loss_dict.values())))
@@ -78,37 +74,39 @@ class rmse(Metric):
 class lat_weighted_mse(Metric):
     def __init__(self, vars, lat, transforms=None, suffix=None, **kwargs):
         super().__init__(**kwargs)
-        self.add_state("preds", default=[], dist_reduce_fx="cat")
-        self.add_state("targets", default=[], dist_reduce_fx="cat")
+        self.add_state("w_mse_over_hw_sum", default=torch.zeros(len(vars)), dist_reduce_fx="sum")
+        self.add_state("count", default=torch.tensor(0), dist_reduce_fx="sum")
+
         self.vars = vars
         self.lat = lat
         self.transforms = transforms
         self.suffix = suffix
+        
+        #latitude weights
+        w_lat = np.cos(np.deg2rad(lat))
+        w_lat = w_lat / w_lat.mean()
+        self.register_buffer("w_lat", torch.from_numpy(w_lat).view(1, 1, -1, 1))  # Shape (1, 1, H, 1)
+
 
     def update(self, preds: torch.Tensor, targets: torch.Tensor):
-        self.preds.append(preds)
-        self.targets.append(targets)
-
-    def compute(self):
-        preds = dim_zero_cat(self.preds)
-        targets = dim_zero_cat(self.targets)
-
         if self.transforms is not None:
             preds = self.transforms(preds)
             targets = self.transforms(targets) 
+        
+        B = preds.shape[0]          
+        error = (preds - targets) ** 2
+        w_mse_over_hw = (error * self.w_lat).mean(dim=(2,3))
+        
+        self.w_mse_over_hw_sum += w_mse_over_hw.sum(dim=0)
+        self.count += B
 
-        error = (preds - targets) ** 2  # [T, V, H, W]
 
-        # lattitude weights
-        w_lat = np.cos(np.deg2rad(self.lat))
-        w_lat = w_lat / w_lat.mean()  # (H, )
-        w_lat = torch.from_numpy(w_lat).unsqueeze(0).unsqueeze(-1).to(dtype=error.dtype, device=error.device)  # (1, H, 1)
-
+    def compute(self):
         loss_dict = {}
 
         for i, var in enumerate(self.vars):
             var_loss_name = f"w_mse_{var}_{self.suffix}" if self.suffix else f"w_mse_{var}"
-            loss_dict[var_loss_name] = (error[:, i] * w_lat).mean()
+            loss_dict[var_loss_name] = self.w_mse_over_hw_sum[i] / self.count
 
         loss_name = f"w_mse_{self.suffix}" if self.suffix else f"w_mse"
         loss_dict[loss_name] = torch.mean(torch.stack(list(loss_dict.values())))
@@ -118,37 +116,38 @@ class lat_weighted_mse(Metric):
 class lat_weighted_rmse(Metric):
     def __init__(self, vars, lat, transforms=None, suffix=None, **kwargs):
         super().__init__(**kwargs)
-        self.add_state("preds", default=[], dist_reduce_fx="cat")
-        self.add_state("targets", default=[], dist_reduce_fx="cat")
+        self.add_state("w_mse_over_hw_sum", default=torch.zeros(len(vars)), dist_reduce_fx="sum")
+        self.add_state("count", default=torch.tensor(0), dist_reduce_fx="sum")
+
         self.vars = vars
         self.lat = lat
         self.transforms = transforms
         self.suffix = suffix
 
+        #latitude weights
+        w_lat = np.cos(np.deg2rad(lat))
+        w_lat = w_lat / w_lat.mean()
+        self.register_buffer("w_lat", torch.from_numpy(w_lat).view(1, 1, -1, 1))  # Shape (1, 1, H, 1)
+
+
     def update(self, preds: torch.Tensor, targets: torch.Tensor):
-        self.preds.append(preds)
-        self.targets.append(targets)
-
-    def compute(self):
-        preds = dim_zero_cat(self.preds)
-        targets = dim_zero_cat(self.targets)
-
         if self.transforms is not None:
             preds = self.transforms(preds)
             targets = self.transforms(targets) 
+        
+        B = preds.shape[0]          
+        error = (preds - targets) ** 2
+        w_mse_over_hw = (error * self.w_lat).mean(dim=(2,3))
+        
+        self.w_mse_over_hw_sum += w_mse_over_hw.sum(dim=0)
+        self.count += B
 
-        error = (preds - targets) ** 2  # [T, V, H, W]
-
-        # lattitude weights
-        w_lat = np.cos(np.deg2rad(self.lat))
-        w_lat = w_lat / w_lat.mean()  # (H, )
-        w_lat = torch.from_numpy(w_lat).unsqueeze(0).unsqueeze(-1).to(dtype=error.dtype, device=error.device)  # (1, H, 1)
-
+    def compute(self):
         loss_dict = {}
 
         for i, var in enumerate(self.vars):
             var_loss_name = f"w_rmse_{var}_{self.suffix}" if self.suffix else f"w_rmse_{var}"
-            loss_dict[var_loss_name] = torch.sqrt((error[:, i] * w_lat).mean())
+            loss_dict[var_loss_name] = torch.sqrt(self.w_mse_over_hw_sum[i] / self.count)
 
         loss_name = f"w_rmse_{self.suffix}" if self.suffix else f"w_rmse"
         loss_dict[loss_name] = torch.mean(torch.stack(list(loss_dict.values())))
@@ -156,39 +155,40 @@ class lat_weighted_rmse(Metric):
         return loss_dict
 
 class lat_weighted_rmse_spatial_map(Metric):
-    def __init__(self, vars, lat, transforms=None, suffix=None, **kwargs):
+    def __init__(self, vars, lat, resolution, transforms=None, suffix=None, **kwargs):
         super().__init__(**kwargs)
-        self.add_state("preds", default=[], dist_reduce_fx="cat")
-        self.add_state("targets", default=[], dist_reduce_fx="cat")
+
+        assert(len(lat) == resolution[0]), f"Found mismatch in size between input latitudes {len(lat)} and specified resolution {resolution}"
+
+        self.add_state("w_mse_spatial_map_sum", default=torch.zeros(len(vars),resolution[0], resolution[1]), dist_reduce_fx="sum")
+        self.add_state("count", default=torch.tensor(0), dist_reduce_fx="sum")
+
         self.vars = vars
         self.lat = lat
         self.transforms = transforms
         self.suffix = suffix
 
+        #latitude weights
+        w_lat = np.cos(np.deg2rad(lat))
+        w_lat = w_lat / w_lat.mean()
+        self.register_buffer("w_lat", torch.from_numpy(w_lat).view(1, 1, -1, 1))  # Shape (1, 1, H, 1)
+
     def update(self, preds: torch.Tensor, targets: torch.Tensor):
-        self.preds.append(preds)
-        self.targets.append(targets)
-
-    def compute(self):
-        preds = dim_zero_cat(self.preds)
-        targets = dim_zero_cat(self.targets)
-
         if self.transforms is not None:
             preds = self.transforms(preds)
             targets = self.transforms(targets) 
-
-        error = (preds - targets) ** 2  # [T, V, H, W]
-
-        # lattitude weights
-        w_lat = np.cos(np.deg2rad(self.lat))
-        w_lat = w_lat / w_lat.mean()  # (H, )
-        w_lat = torch.from_numpy(w_lat).unsqueeze(0).unsqueeze(-1).to(dtype=error.dtype, device=error.device)  # (1, H, 1)
-
+        
+        B = preds.shape[0]          
+        error = (preds - targets) ** 2
+        self.w_mse_spatial_map_sum += (error * self.w_lat).sum(dim=0)
+        self.count += B
+    
+    def compute(self):
         spatial_map_dict = {}
 
         for i, var in enumerate(self.vars):
             spatial_map_name = f"w_rmse_spatial_{var}_{self.suffix}" if self.suffix else f"w_rmse_spatial_map_{var}"
-            spatial_map_dict[spatial_map_name] = torch.sqrt((error[:, i] * w_lat).mean(dim=(0)))
+            spatial_map_dict[spatial_map_name] = torch.sqrt(self.w_mse_spatial_map_sum[i] / self.count)
 
         spatial_map_name = f"w_rmse_spatial_{self.suffix}" if self.suffix else f"w_rmse"
         spatial_map_dict[spatial_map_name] = torch.mean(torch.stack(list(spatial_map_dict.values())), dim=(0))
@@ -197,57 +197,43 @@ class lat_weighted_rmse_spatial_map(Metric):
 
 
 class lat_weighted_acc(Metric):
-    def __init__(self, vars, lat, clim, clim_timestamps, transforms=None, suffix=None, **kwargs):
+    def __init__(self, vars, lat, transforms=None, suffix=None, **kwargs):
         super().__init__(**kwargs)
-        self.add_state("preds", default=[], dist_reduce_fx="cat")
-        self.add_state("targets", default=[], dist_reduce_fx="cat")
-        self.add_state("encoded_timestamps", default=[], dist_reduce_fx="cat")
+        self.add_state("w_acc_over_hw_sum", default=[], dist_reduce_fx="cat")
+        self.add_state("count", default=torch.tensor(0), dist_reduce_fx="sum")
+
         self.vars = vars
         self.lat = lat
         self.transforms = transforms
         self.suffix = suffix
-        self.clim = clim
-        self.clim_timestamps = clim_timestamps
+        
+        #latitude weights
+        w_lat = np.cos(np.deg2rad(lat))
+        w_lat = w_lat / w_lat.mean()
+        self.register_buffer("w_lat", torch.from_numpy(w_lat).view(1, 1, -1, 1))  # Shape (1, 1, H, 1)
 
-    def update(self, preds: torch.Tensor, targets: torch.Tensor, output_timestamps: list):
-        self.preds.append(preds)
-        self.targets.append(targets)
-        encoded_timestamps = torch.tensor([encode_timestamp(ts) for ts in output_timestamps], device=preds.device)
-        self.encoded_timestamps.append(encoded_timestamps)
-    
-
-    def compute(self):
-        preds = dim_zero_cat(self.preds)
-        targets = dim_zero_cat(self.targets)
-        encoded_timestamps = dim_zero_cat(self.encoded_timestamps)  
-        decoded_timestamps = [decode_timestamp(ts.item()) for ts in encoded_timestamps]
-        decoded_timestamps_no_year = [remove_year(ts) for ts in decoded_timestamps]
-
+    def update(self, preds: torch.Tensor, targets: torch.Tensor, climatology: torch.Tensor):
+        assert preds.shape == climatology.shape
         if self.transforms is not None:
             preds = self.transforms(preds)
             targets = self.transforms(targets) 
+        
+        B = preds.shape[0]          
+        preds = preds - climatology
+        targets = targets - climatology
+        
+        w_acc_over_hw = torch.sum((self.w_lat * preds * targets), dim=(2,3)) / torch.sqrt(
+            torch.sum((self.w_lat * preds**2), dim=(2,3)) * torch.sum((self.w_lat * targets**2), dim=(2,3)))
+        
+        self.w_acc_over_hw_sum += w_acc_over_hw.sum(dim=0)
+        self.count += B
 
-        # lattitude weights
-        w_lat = np.cos(np.deg2rad(self.lat))
-        w_lat = w_lat / w_lat.mean()  # (H, )
-        w_lat = torch.from_numpy(w_lat).unsqueeze(0).unsqueeze(-1).to(dtype=targets.dtype, device=targets.device)  # (1, H, 1)
-
-        clim_timestamp_to_index = {timestamp: idx for idx, timestamp in enumerate(self.clim_timestamps)}
-        clim_subset_indices = [clim_timestamp_to_index[timestamp] for timestamp in decoded_timestamps_no_year]
-        clim_subset = self.clim[clim_subset_indices]
-        clim_subset = clim_subset.to(device=targets.device)
-        assert preds.shape == clim_subset.shape
-        preds = preds - clim_subset
-        targets = targets - clim_subset
+    def compute(self):
         loss_dict = {}
 
         for i, var in enumerate(self.vars):
-            pred_prime = preds[:, i]
-            targ_prime = targets[:, i]
-            acc = torch.sum((w_lat * pred_prime * targ_prime), dim=(1,2)) / torch.sqrt(
-                torch.sum((w_lat * pred_prime**2), dim=(1,2)) * torch.sum((w_lat * targ_prime**2), dim=(1,2)))
             var_loss_name = f"w_acc_{var}_{self.suffix}" if self.suffix else f"w_acc_{var}"
-            loss_dict[var_loss_name] = acc.mean()
+            loss_dict[var_loss_name] = self.w_acc_over_hw_sum[i] / self.count
 
         loss_name = f"w_acc_{self.suffix}" if self.suffix else f"w_acc"
         loss_dict[loss_name] = torch.mean(torch.stack(list(loss_dict.values())))
@@ -255,56 +241,43 @@ class lat_weighted_acc(Metric):
         return loss_dict
     
 class lat_weighted_acc_spatial_map(Metric):
-    def __init__(self, vars, lat, clim, clim_timestamps, transforms=None, suffix=None, **kwargs):
+    def __init__(self, vars, lat, resolution, transforms=None, suffix=None, **kwargs):
         super().__init__(**kwargs)
-        self.add_state("preds", default=[], dist_reduce_fx="cat")
-        self.add_state("targets", default=[], dist_reduce_fx="cat")
-        self.add_state("encoded_timestamps", default=[], dist_reduce_fx="cat")
+        assert(len(lat) == resolution[0]), f"Found mismatch in size between input latitudes {len(lat)} and specified resolution {resolution}"
+
+        self.add_state("w_acc_spatial_map_sum_tp", default=torch.zeros(len(vars),resolution[0], resolution[1]), dist_reduce_fx="sum")
+        self.add_state("w_acc_spatial_map_sum_pp", default=torch.zeros(len(vars),resolution[0], resolution[1]), dist_reduce_fx="sum")
+        self.add_state("w_acc_spatial_map_sum_tt", default=torch.zeros(len(vars),resolution[0], resolution[1]), dist_reduce_fx="sum")
+        
         self.vars = vars
         self.lat = lat
         self.transforms = transforms
         self.suffix = suffix
-        self.clim = clim
-        self.clim_timestamps = clim_timestamps
+        
+        #latitude weights
+        w_lat = np.cos(np.deg2rad(lat))
+        w_lat = w_lat / w_lat.mean()
+        self.register_buffer("w_lat", torch.from_numpy(w_lat).view(1, 1, -1, 1))  # Shape (1, 1, H, 1)
 
-    def update(self, preds: torch.Tensor, targets: torch.Tensor, output_timestamps: list):
-        self.preds.append(preds)
-        self.targets.append(targets)
-        encoded_timestamps = torch.tensor([encode_timestamp(ts) for ts in output_timestamps], device=preds.device)
-        self.encoded_timestamps.append(encoded_timestamps)
-    
-    def compute(self):
-        preds = dim_zero_cat(self.preds)
-        targets = dim_zero_cat(self.targets)
-        encoded_timestamps = dim_zero_cat(self.encoded_timestamps)  
-        decoded_timestamps = [decode_timestamp(ts.item()) for ts in encoded_timestamps]
-        decoded_timestamps_no_year = [remove_year(ts) for ts in decoded_timestamps]
-
+    def update(self, preds: torch.Tensor, targets: torch.Tensor, climatology: torch.Tensor):
+        assert preds.shape == climatology.shape
         if self.transforms is not None:
             preds = self.transforms(preds)
             targets = self.transforms(targets) 
-
-        # lattitude weights
-        w_lat = np.cos(np.deg2rad(self.lat))
-        w_lat = w_lat / w_lat.mean()  # (H, )
-        w_lat = torch.from_numpy(w_lat).unsqueeze(0).unsqueeze(-1).to(dtype=targets.dtype, device=targets.device)  # (1, H, 1)
-
-        clim_timestamp_to_index = {timestamp: idx for idx, timestamp in enumerate(self.clim_timestamps)}
-        clim_subset_indices = [clim_timestamp_to_index[timestamp] for timestamp in decoded_timestamps_no_year]
-        clim_subset = self.clim[clim_subset_indices]
-        clim_subset = clim_subset.to(device=targets.device)
-        assert preds.shape == clim_subset.shape
-        preds = preds - clim_subset
-        targets = targets - clim_subset
+        
+        preds = preds - climatology
+        targets = targets - climatology
+        
+        self.w_acc_spatial_map_sum_tp += torch.sum((self.w_lat * preds * targets), dim=(0)) 
+        self.w_acc_spatial_map_sum_pp += torch.sum((self.w_lat * preds **2), dim=(0)) 
+        self.w_acc_spatial_map_sum_tt += torch.sum((self.w_lat * targets **2), dim=(0)) 
+    
+    def compute(self):
         spatial_map_dict = {}
 
         for i, var in enumerate(self.vars):
-            pred_prime = preds[:, i]
-            targ_prime = targets[:, i]
-            acc_spatial_map = torch.sum((w_lat * pred_prime * targ_prime), dim=(0)) / torch.sqrt(
-                torch.sum((w_lat * pred_prime**2), dim=(0)) * torch.sum((w_lat * targ_prime**2), dim=(0)))
             spatial_map_name = f"w_acc_spatial_{var}_{self.suffix}" if self.suffix else f"w_acc_spatial_{var}"
-            spatial_map_dict[spatial_map_name] = acc_spatial_map
+            spatial_map_dict[spatial_map_name] = self.w_acc_spatial_map_sum_tp[i] / torch.sqrt(self.w_acc_spatial_map_sum_pp[i] * self.w_acc_spatial_map_sum_tt[i])
 
         spatial_map_name = f"w_acc_spatial_{self.suffix}" if self.suffix else f"w_acc"
         spatial_map_dict[spatial_map_name] = torch.mean(torch.stack(list(spatial_map_dict.values())), dim=(0))
